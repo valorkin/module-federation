55fd3ea40574d342ca5941badf5295f94472b78a
nowant <nowant@rambler.ru>
2021-01-19 17:34:18 +0200

    add some fixes, add comments and etc

--- src/builder.ts
+++ src/builder.ts
@@ -5,54 +5,75 @@ import { mergeDeep, traverse } from './object.util';
 import { readFileAsJson, writeFileAsJson } from './file.util';
 import { BuilderOptions, JsonObjectMaps } from './builder.interface';
 
-const srcPattern = /^src$|^src\/|^\/src\/|^\.\/src\//;
-const fileExtensionsPattern = /\.js$|\.ts$|\.json$|\.html$/;
-
+/**
+ * Helper function
+ */
 const prepandRootPath = (path: string, rootPath: string): string => {
   const cleanPath = path.replace(/^\.\/|^\//, '');
   return `${rootPath}/${cleanPath}`;
 }
 
+/**
+ * Helper function
+ */
 const overrideLocalToRootPaths = (localProjectAngularJson: JsonObject, localRootPath: string) => {
-  traverse(localProjectAngularJson, (pathKey: string, path: any, nestedProperty: any) => {
+  // Regexp pattern to define source paths `src/*`
+  const srcPattern = /^src$|^src\/|^\/src\/|^\.\/src\//;
+  // Regexp pattern to define file paths such as `tsconfig.app.json`
+  const fileExtensionsPattern = /\.[^/.]+$/;
+
+  traverse(localProjectAngularJson, (targetPropertyKey: string, path: any, targetProperty: any) => {
     if (typeof path !== 'string') {
       return;
     }
 
     if (srcPattern.test(path)) {
-      nestedProperty[pathKey] = prepandRootPath(path, localRootPath);
+      targetProperty[targetPropertyKey] = prepandRootPath(path, localRootPath);
       return;
     }
 
     if (fileExtensionsPattern.test(path)) {
-      nestedProperty[pathKey] = prepandRootPath(path, localRootPath);
+      targetProperty[targetPropertyKey] = prepandRootPath(path, localRootPath);
     }
   });
 }
 
+/**
+ * Builder implementation
+ */
 function builder(options: BuilderOptions) {
   return new Promise<BuilderOutput>(async (resolve, reject) => {
-    console.log(`Expanding project: '${options.project}'`);
+    console.log(`Expanding '${options.project}' project...`);
 
+    // getting root `angular.json`
     const rootAngularJsonPath = 'angular.json';
     const rootAngularJson = await readFileAsJson(rootAngularJsonPath)
       .catch(error => reject(error)) as JsonObjectMaps;
 
+    // getting local project's config from root `angular.json`
     const rootProjectAngularJson = rootAngularJson.projects[options.project] as JsonObject;
 
+    // getting local project's `projects/project/angular.json`
     const localRootPath = `${rootAngularJson.newProjectRoot}/${options.project}`;
     const localAngularJsonPath = `${localRootPath}/${rootAngularJsonPath}`;
     const localAngularJson = await readFileAsJson(localAngularJsonPath)
       .catch(error => reject(error)) as JsonObjectMaps;
 
+    // getting local project's config from local `projects/project/angular.json`
     const localProjectAngularJson = localAngularJson.projects[options.project] as JsonObject;
 
+    // overriding local paths `src/` into root paths `projects/project/src`
+    // overriding path to files `tsconfig.app.json` into root paths `projects/project/tsconfig.app.json`
     overrideLocalToRootPaths(localProjectAngularJson, localRootPath);
 
+    // merging local `projects/project/angular.json` into root `angular.json`
     rootAngularJson.projects[options.project] = mergeDeep(localProjectAngularJson, rootProjectAngularJson);
 
     writeFileAsJson(rootAngularJsonPath, rootAngularJson)
-      .then(() => resolve({ success: true }))
+      .then(() => {
+        console.log(`Expanding of '${options.project}' project has successfully finished.`)
+        resolve({ success: true });
+      })
       .catch(error => reject(error));
   });
 };
--- src/object.util.ts
+++ src/object.util.ts
@@ -18,9 +18,7 @@ export const mergeDeep = (target: any, ...sources: any[]): any => {
   if (isObject(target) && isObject(source)) {
     for (const key in source) {
       if (isObject(source[key])) {
-        if (!target[key]) {
-          target[key] = {};
-        }
+        target[key] = target[key] || {};
         mergeDeep(target[key], source[key]);
       } else {
         Object.assign(target, { [key]: source[key] });
