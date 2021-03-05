951d0be20d0b31ba928427473dec2e9e0b8afaa5
nowant <nowant@rambler.ru>
2021-03-05 13:22:48 +0200

    create extract-i18n command

--- src/extract-i18n/index.ts
+++ src/extract-i18n/index.ts
@@ -1,19 +1,18 @@
 import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
-import { serveWebpackBrowserPlus } from 'ngx-build-plus/src/plus-dev-server';
+import { execute } from '@angular-devkit/build-angular/src/extract-i18n';
 import { Transforms } from 'ngx-build-plus/src/utils';
 import { expandWorkspaceOptions, overrideBuilderContext } from '../utils';
 
 /**
  * Builder implementation
  */
-export async function serveWebpackBrowser(options: any, context: BuilderContext, transforms: Transforms = {}): Promise<BuilderOutput> {
+export async function extractI18nWebpack(options: any, context: BuilderContext, transforms: Transforms = {}): Promise<BuilderOutput> {
   const expandedOptions = await expandWorkspaceOptions(options, context)
     .catch(error => console.log(error));
 
   const overridedContext = overrideBuilderContext(expandedOptions.browserTarget, context);
-  return serveWebpackBrowserPlus(expandedOptions, overridedContext, transforms)
-    .toPromise();
+  return execute(expandedOptions, overridedContext, transforms);
 }
 
-export default createBuilder(serveWebpackBrowser);
+export default createBuilder(extractI18nWebpack);
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './object.util';
 export * from './file.util';
 export * from './expander.util';
+export * from './context.util';
