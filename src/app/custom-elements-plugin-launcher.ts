86aaff736af0d5853226985b1155d82a31483ef9
Dmitriy Shekhovtsov <valorkin@gmail.com>
2021-03-22 15:27:05 +0200

    chore: clean trash

--- src/app/custom-elements-plugin-launcher.ts
+++ src/app/custom-elements-plugin-launcher.ts
@@ -1,24 +1,18 @@
 import { Injector, NgModule } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-import { AlertModule } from 'ngx-bootstrap/alert';
-import { AppComponent } from './app.component';
 import { PluginLauncherComponent } from './plugin-launcher/plugin-launcher.component';
 import { PluginLauncherModule } from './plugin-launcher/plugin-launcher.module';
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
     BrowserModule,
-    PluginLauncherModule,
-    AlertModule
+    PluginLauncherModule
   ],
-  providers: [],
-  entryComponents: [PluginLauncherComponent]
 })
-export class AppModule {
+export class CustomElementsPluginLauncher {
   constructor(readonly injector: Injector) {
     const ngElement = createCustomElement(PluginLauncherComponent, {injector});
     customElements.define('ngce-alert', ngElement);
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { CustomElementsPluginLauncher } from './app/custom-elements-plugin-launcher';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(CustomElementsPluginLauncher)
   .catch(err => console.error(err));
