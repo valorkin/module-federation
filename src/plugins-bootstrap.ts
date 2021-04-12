3d32aac221e42d790da40865363452b823b17aa6
Dmitriy Shekhovtsov <valorkin@gmail.com>
2021-04-12 18:40:15 +0300

    chore: plugin.json should be optional

--- src/plugins-bootstrap.ts
+++ src/plugins-bootstrap.ts
@@ -7,17 +7,10 @@ export const loadPluginsJson = (): Promise<void> => {
     const pathToFile = './assets/config/plugins.json';
     const response = await window.fetch(pathToFile) as Response;
 
-    if (!response.ok) {
-      reject();
-    }
+    const json = response.ok ? await response.json() : [];
+    const windowConfig = Array.isArray(window.__appShellPluginsJson__) ? window.__appShellPluginsJson__ : [];
 
-    const json = await response.json();
-
-    if (Array.isArray(json)) {
-      window.__appShellPluginsJson__ = json;
-      return resolve();
-    }
-
-    reject();
+    window.__appShellPluginsJson__ = json.concat(windowConfig);
+    return resolve();
   });
-}
+};
