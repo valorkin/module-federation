e36576fb79cdb802ae295b7207f70a1656a30277
nowant <nowant@rambler.ru>
2021-04-12 19:03:39 +0300

    update content-recommended-categories to ng11

--- packages/content-recommended-categories/src/app/recommended-categories/recommended-categories.component.spec.ts
+++ packages/content-recommended-categories/src/app/recommended-categories/recommended-categories.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RecommendedCategoriesComponent } from './recommended-categories.component';
 
@@ -6,7 +6,7 @@ describe('RecommendedCategoriesComponent', () => {
   let component: RecommendedCategoriesComponent;
   let fixture: ComponentFixture<RecommendedCategoriesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RecommendedCategoriesComponent ]
     })
