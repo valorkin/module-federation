1ac4e812f33e2e850287c84b3c227695d667bc6c
nowant <nowant@rambler.ru>
2021-04-14 13:14:18 +0300

    create demo with resolving MF assets path issue

--- packages/content-recommended-categories/src/app/recommended-categories/recommended-categories.component.ts
+++ packages/content-recommended-categories/src/app/recommended-categories/recommended-categories.component.ts
@@ -1,7 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Optional, Inject } from '@angular/core';
+import { REMOTE_BASE_URL } from '@fundamental-ngx/app-shell';
 @Component({
   selector: 'app-recommended-categories',
-  templateUrl: './recommended-categories.component.html'
+  templateUrl: './recommended-categories.component.html',
+  styleUrls: ['./recommended-categories.component.scss']
 })
-export class RecommendedCategoriesComponent {}
+export class RecommendedCategoriesComponent implements OnInit {
+  dynamicImgSrc: string;
+  dynamicImgSize = 'w320';
+
+  constructor(
+    @Optional()
+    @Inject(REMOTE_BASE_URL)
+    public baseUrl: string
+  ) {}
+
+  ngOnInit(): void {
+    this.dynamicImgSrc = `assets/nature-${this.dynamicImgSize}.jpg`;
+  }
+}
