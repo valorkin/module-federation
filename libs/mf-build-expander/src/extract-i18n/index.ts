d287d2196bbe035673fd94f291ab8ba29d864383
nowant <nowant@rambler.ru>
2021-10-28 03:17:03 +0300

    update mf-build-expander to work with nodejs >= 14

--- libs/mf-build-expander/src/extract-i18n/index.ts
+++ libs/mf-build-expander/src/extract-i18n/index.ts
@@ -1,12 +1,12 @@
-import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
-import { execute } from '@angular-devkit/build-angular/src/extract-i18n';
-import { Transforms } from 'ngx-build-plus/src/utils';
-import { expandWorkspaceOptions, overrideBuilderContext } from '../utils';
+//import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
+//import { execute } from '@angular-devkit/build-angular/src/extract-i18n';
+//import { Transforms } from 'ngx-build-plus/src/utils';
+//import { expandWorkspaceOptions, overrideBuilderContext } from '../utils';
 
 /**
  * Builder implementation
  */
-export async function extractI18nWebpack(options: any, context: BuilderContext, transforms: Transforms = {}): Promise<BuilderOutput> {
+/*export async function extractI18nWebpack(options: any, context: BuilderContext, transforms: Transforms = {}): Promise<BuilderOutput> {
   const expandedOptions = await expandWorkspaceOptions(options, context)
     .catch(error => console.log(error));
 
@@ -14,5 +14,5 @@ export async function extractI18nWebpack(options: any, context: BuilderContext, 
   return execute(expandedOptions, overridedContext, transforms);
 }
 
-export default createBuilder(extractI18nWebpack);
+export default createBuilder(extractI18nWebpack);*/
 
